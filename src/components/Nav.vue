91d8112eeb3c953a430cad54546fe182fd7a1659
Jimena Torres <jimenatorres.10@gmail.com>
2020-04-28 22:50:59 -0500

    CORE casi listo

---
+++ src/components/Nav.vue
@@ -0,0 +1,23 @@
+<template>
+  <aside>
+    <a class="nav_logo" href="#">LOGO</a>
+    <nav>
+      <ul>
+        <li>Inicio</li>
+        <li>Mensajes</li>
+        <li>Mi cuenta</li>
+      </ul>
+    </nav>
+    <h2>Cerrar sesión</h2>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+};
+</script>
+
+<style>
+
+</style>
